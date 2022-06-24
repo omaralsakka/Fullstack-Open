99bc00be8f6ce10126c5d399b6043297b1653a8d
Omar Alsakka <djomrofficial@gmail.com>
2022-06-24 17:27:39 +0300

    step1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const Persons = ({ persons }) => {
+  return persons.map((person) => {
+    return <div key={person.id}>{person.name}</div>;
+  });
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", id: Math.random(1, 9999) },
+  ]);
+
+  const [newName, setNewName] = useState("");
+
+  const handleNewPerson = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newObj = {
+      name: newName,
+      id: Math.random(1, 9999),
+    };
+    setPersons(persons.concat(newObj));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewPerson} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <br></br>
+      <Persons persons={persons} />
+      <div>debug: {newName}</div>
+    </div>
+  );
+};
+
+export default App;
