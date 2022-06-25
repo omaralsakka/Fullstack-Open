83b5d5fbfd71a3548e58566fa0b7ac14e70c1ef3
Omar Alsakka <djomrofficial@gmail.com>
2022-06-25 11:26:00 +0300

    step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
-const Persons = ({ persons }) => {
+const Header = ({ title }) => <h2>{title}</h2>;
+
+const PersonDisplay = ({ persons }) => {
   return persons.map((person) => {
     return (
       <div key={person.id}>
@@ -10,13 +12,34 @@ const Persons = ({ persons }) => {
   });
 };
 
+const Persons = ({ persons, filter }) => {
+  if (filter !== "") {
+    const filtered = persons.filter((person) =>
+      person.name.toLowerCase().includes(filter)
+    );
+    return <PersonDisplay persons={filtered} />;
+  } else return <PersonDisplay persons={persons} />;
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567", id: Math.random(1, 9999) },
+    { name: "Arto Hellas", number: "040-123456", id: Math.random(1, 9999) },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: Math.random(1, 9999) },
+    { name: "Dan Abramov", number: "12-43-234345", id: Math.random(1, 9999) },
+    {
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+      id: Math.random(1, 9999),
+    },
   ]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState("");
+
+  const handleNewFilter = (event) => {
+    setFilterName(event.target.value);
+  };
 
   const handleNewPerson = (event) => {
     setNewName(event.target.value);
@@ -43,7 +66,14 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <Header title="Phonebook" />
+      <div>
+        filter shown with:{" "}
+        <input value={filterName} onChange={handleNewFilter} />
+      </div>
+
+      <Header title="add a new" />
+
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNewPerson} />
@@ -55,11 +85,9 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <br></br>
-      <Persons persons={persons} />
-      {/* <div>debug: {newName}</div>
-      <div>debug: {newNumber}</div> */}
+
+      <Header title="Numbers" />
+      <Persons persons={persons} filter={filterName} />
     </div>
   );
 };
