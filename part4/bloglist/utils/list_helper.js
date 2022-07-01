23730ac42be4daf164204ea226054d5a3d5d9de9
Omar Alsakka <djomrofficial@gmail.com>
2022-07-01 19:47:40 +0300

    4.4 step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,10 @@ const dummy = (blogs) => {
   return blogs ? 1 : 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes;
+  }, 0);
+};
+
+module.exports = { dummy, totalLikes };
