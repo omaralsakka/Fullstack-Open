5251ddaec0b57648536cc6bcd6475f75f38f5533
Omar Alsakka <djomrofficial@gmail.com>
2022-07-01 17:23:59 +0300

    step1 & step2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,22 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const blogsRouter = require("./controllers/blogs");
+
+mongoose
+  .connect(config.mongoUrl)
+  .then(() => {
+    logger.info("connected to Mongodb");
+  })
+  .catch((error) => {
+    logger.error("error connecting to mongodb", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+module.exports = app;
