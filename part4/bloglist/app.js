b0dadc06463cc3ea8e721f81035ec5438e9d1142
Omar Alsakka <djomrofficial@gmail.com>
2022-07-01 18:05:49 +0300

    step2 still, moved mongoose into its own file, required only in app

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,20 +1,10 @@
-const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
-const logger = require("./utils/logger");
 const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleWare");
 
-mongoose
-  .connect(config.mongoUrl)
-  .then(() => {
-    logger.info("connected to Mongodb");
-  })
-  .catch((error) => {
-    logger.error("error connecting to mongodb", error.message);
-  });
+require("./utils/mongoose");
 
 app.use(cors());
 app.use(express.json());
