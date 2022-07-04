503807173ac0ef1ab8d9d4c3c46d604eff9cfc7a
Omar Alsakka <djomrofficial@gmail.com>
2022-07-04 19:15:00 +0300

    4.18 step6

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleWare");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 require("./tests/test_helper");
 
 require("./utils/mongoose");
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
