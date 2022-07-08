eac4df1c83e775e8a04d5f8d30ee412f2f41d9ce
Omar Alsakka <djomrofficial@gmail.com>
2022-07-08 12:06:59 +0300

    added README for all

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleWare");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+
 require("./tests/test_helper");
 
 require("./utils/mongoose");
@@ -15,6 +16,12 @@ app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
