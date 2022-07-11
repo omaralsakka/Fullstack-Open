5c0bc1c680252b66d3bb094393c54ce139a323f7
Omar Alsakka <djomrofficial@gmail.com>
2022-07-11 12:13:41 +0300

    6.21 grand finale

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+let timer;
+
 const notificationSlice = createSlice({
   name: "notifications",
   initialState: "",
@@ -14,8 +16,9 @@ export const { Notification } = notificationSlice.actions;
 
 export const setNotification = (content, time) => {
   return async (dispatch) => {
-    await dispatch(Notification(`you voted for ${content}`));
-    setTimeout(() => {
+    clearTimeout(timer);
+    dispatch(Notification(`you voted for ${content}`));
+    timer = setTimeout(() => {
       dispatch(Notification(""));
     }, time * 1000);
   };
